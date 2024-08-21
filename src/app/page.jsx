e4f058ddb8bdb2cd1706152a375d4499aa600104
dahalapaar <contact.apaardahal@gmail.com>
2024-08-21 15:50:12 +0800

    home page design complete

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,5 +2,27 @@ import Image from "next/image"
 import styles from "./page.module.css"
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.description}>
+          Turning your ideas into Reality. We bring together the teams from the
+          gloabl tech industry
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image
+          src="/hero.png"
+          alt="Home"
+          width={400}
+          height={300}
+          className={styles.img}
+        />
+      </div>
+    </div>
+  )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react"
+import styles from "./footer.module.css"
+import Image from "next/image"
 
 const Footer = () => {
   return (
-    <div>
-      <div>@copyright Apaar Dahal</div>
-      <div></div>
+    <div className={styles.container}>
+      <div className={styles.text}>@copyright Apaar Dahal</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          alt="apaar"
+          className={styles.icon}
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          alt="apaar"
+          className={styles.icon}
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          alt="apaar"
+          className={styles.icon}
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          alt="apaar"
+          className={styles.icon}
+        />
+      </div>
     </div>
   )
 }
