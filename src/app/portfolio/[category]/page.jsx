a59614c73e34f070a3fd9d35de19e37bb5d24343
dahalapaar <contact.apaardahal@gmail.com>
2024-09-01 11:36:50 +0800

    portfolio page design complete

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,46 @@
 import React from "react"
+import styles from "./page.module.css"
+import Button from "@/components/button/Button"
+import Image from "next/image"
 
-const Category = () => {
-  return <div>Category</div>
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.items}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/hero.png"
+            alt="Home"
+            width={300}
+            height={300}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.items}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/hero.png"
+            alt="Home"
+            width={300}
+            height={300}
+            className={styles.img}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Category
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import styles from "./page.module.css"
+import Link from "next/link"
 
 const Portfolio = () => {
-  return <div className={styles.container}>Portfolio</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.Stitle}>Chose a gallery</div>
+      <div className={styles.items}>
+        <Link className={styles.item} href="/portfolio/illustrations">
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link className={styles.item} href="/portfolio/websites">
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link className={styles.item} href="/portfolio/applications">
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Portfolio
